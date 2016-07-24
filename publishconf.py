ccdd78a13348b7449a194389871461163abce6ab
Mike Henry <mhenry@handycodejob.com>
2016-07-24 13:57:55 -0600

    start of pelican stuff

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://mikemhenry.github.io/arcade'
+SITEURL = 'http://henrymike.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
